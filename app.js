7fc24f5856e611b7f09b3fdf5fd1798d8d10aba7
Mujhtech <mujeeb.muhideen@gmail.com>
2021-01-21 11:51:13 +0100

    let's heroku set port

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.get("/api/rates", (req, res) => {
     });
   }
 });
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
